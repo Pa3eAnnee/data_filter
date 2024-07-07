75470ab880ae732a849f8fbc004e26e8d3aafb45
vanande <vkhatchatrian1@myges.fr>
2024-07-07 17:06:46 +0100

    fix data processing

--- tests/TestDataProcessing.py
+++ tests/TestDataProcessing.py
@@ -7,11 +7,11 @@ class TestDataProcessing(unittest.TestCase):
     def test_process_file(self):
         raw_data = read_file("data/test_data_short.csv")
         data = process_file(raw_data)
-        self.assertEqual(data[0]["firstname"], "John")
-        self.assertEqual(data[0]["lastname"], "Doe")
-        self.assertEqual(data[0]["age"], 25)
-        self.assertEqual(data[0]["apprentice"], True)
-        self.assertEqual(data[0]["grades"], [85, 90, 95])
+        self.assertEqual(data["firstname"], "John")
+        self.assertEqual(data["lastname"], "Doe")
+        self.assertEqual(data["age"], "25")
+        self.assertEqual(data["apprentice"], "True")
+        self.assertEqual(data["grades"], "[85,90,95]")
 
 if __name__ == "__main__":
     unittest.main()

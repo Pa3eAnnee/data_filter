7ab20a7eb0e23a154b86bb4c50f3a367e9acea48
v.khatchatrian <v.khatchatrian@groupeonepoint.com>
2024-07-15 14:48:04 +0200

    add: test xml

--- tests/TestDataProcessing.py
+++ tests/TestDataProcessing.py
@@ -1,23 +1,44 @@
 import unittest
+import os
 
 from src.data_io import read_file
 from src.data_processing import process_file
 
 class TestDataProcessing(unittest.TestCase):
-    def test_process_file_short(self):
-        data = process_file("C:\\PA\\python\\data_filter\\tests\\data\\test_data_short.csv")
+    def setUp(self):
+        self.base_path = "C:\\PA\\python\\data_filter\\tests\\data"
+
+    def test_process_file_short_csv(self):
+        data = process_file(os.path.join(self.base_path, "test_data_short.csv"))
         self.assertEqual(data[0]["firstname"], "John")
         self.assertEqual(data[0]["lastname"], "Doe")
-        self.assertEqual(data[0]["age"], "25")
-        self.assertEqual(data[0]["apprentice"], "True")
+        self.assertEqual(data[0]["age"], 25)
+        self.assertEqual(data[0]["apprentice"], True)
         self.assertEqual(data[0]["grades"], "[85,90,95]")
 
-    def test_process_file(self):
-        data = process_file("C:\\PA\\python\\data_filter\\tests\\data\\test_data.csv")
+    def test_process_file_csv(self):
+        data = process_file(os.path.join(self.base_path, "test_data.csv"))
         self.assertEqual(data[0]["firstname"], "John")
         self.assertEqual(data[0]["lastname"], "Doe")
-        self.assertEqual(data[0]["age"], "25")
-        self.assertEqual(data[0]["apprentice"], "True")
+        self.assertEqual(data[0]["age"], 25)
+        self.assertEqual(data[0]["apprentice"], True)
+        self.assertEqual(data[0]["grades"], "[85,90,95]")
+        self.assertEqual(data[1]["firstname"], "Jane")
+
+    def test_process_file_short_xml(self):
+        data = process_file(os.path.join(self.base_path, "test_data_short.xml"))
+        self.assertEqual(data[0]["firstname"], "John")
+        self.assertEqual(data[0]["lastname"], "Doe")
+        self.assertEqual(data[0]["age"], 25)
+        self.assertEqual(data[0]["apprentice"], True)
+        self.assertEqual(data[0]["grades"], "[85,90,95]")
+
+    def test_process_file_xml(self):
+        data = process_file(os.path.join(self.base_path, "test_data.xml"))
+        self.assertEqual(data[0]["firstname"], "John")
+        self.assertEqual(data[0]["lastname"], "Doe")
+        self.assertEqual(data[0]["age"], 25)
+        self.assertEqual(data[0]["apprentice"], True)
         self.assertEqual(data[0]["grades"], "[85,90,95]")
         self.assertEqual(data[1]["firstname"], "Jane")
 
